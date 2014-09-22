bdee6966ac04b21a92326368df186f45fdc768bd
Aaron Silvas <asilvas@godaddy.com>
2014-09-22 08:08:10 -0700

    update bench based on pathing changes

--- scripts/bench-doc-puts.bat
+++ scripts/bench-doc-puts.bat
@@ -1,1 +1,1 @@
-ab -T "application/json" -c 20 -n 50000 -u c:\tmp\put.json http://localhost:11911/table/testTable/doc/testDocument
+ab -T "application/json" -c 20 -n 50000 -u c:\tmp\put.json http://localhost:11911/db/testDB/table/testTable/doc/testPart/testRow
